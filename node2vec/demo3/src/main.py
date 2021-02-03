13c9b3009c1d7260ac9b2d4e95faea3d1eb08dab
jiangpeng.jiang <jiangpeng.jiang@zhaopin.com.cn>
2021-02-03 16:02:29 +0800

    update node2vec

--- node2vec/demo3/src/main.py
+++ node2vec/demo3/src/main.py
@@ -29,35 +29,22 @@ def parse_args():
 	Parses the node2vec arguments.
 	'''
 	parser = argparse.ArgumentParser(description="Run node2vec.")
-
 	parser.add_argument('--input', nargs='?', default='graph/karate.edgelist', help='Input graph path')
-
 	parser.add_argument('--output', nargs='?', default='emb/karate.emb', help='Embeddings path')
-
 	parser.add_argument('--dimensions', type=int, default=128, help='Number of dimensions. Default is 128.')
-
 	parser.add_argument('--walk-length', type=int, default=80, help='Length of walk per source. Default is 80.')
-
 	parser.add_argument('--num-walks', type=int, default=10, help='Number of walks per source. Default is 10.')
-
 	parser.add_argument('--window-size', type=int, default=10, help='Context size for optimization. Default is 10.')
-
 	parser.add_argument('--iter', default=1, type=int, help='Number of epochs in SGD')
-
 	parser.add_argument('--workers', type=int, default=8, help='Number of parallel workers. Default is 8.')
-
 	parser.add_argument('--p', type=float, default=1,  help='Return hyperparameter. Default is 1.')
-
 	parser.add_argument('--q', type=float, default=1, help='Inout hyperparameter. Default is 1.')
-
 	parser.add_argument('--weighted', dest='weighted', action='store_true', help='Boolean specifying (un)weighted. Default is unweighted.')
 	parser.add_argument('--unweighted', dest='unweighted', action='store_false')
 	parser.set_defaults(weighted=False)
-
 	parser.add_argument('--directed', dest='directed', action='store_true', help='Graph is (un)directed. Default is undirected.')
 	parser.add_argument('--undirected', dest='undirected', action='store_false')
 	parser.set_defaults(directed=False)
-
 	return parser.parse_args()
 
 
@@ -66,7 +53,7 @@ def read_graph():
 	Reads the input network in networkx.
 	'''
 	if args.weighted:
-		G = nx.read_edgelist(args.input, nodetype=int, data=(('weight',float),), create_using=nx.DiGraph())
+		G = nx.read_edgelist(args.input, nodetype=int, data=(('weight', float),), create_using=nx.DiGraph())
 	else:
 		G = nx.read_edgelist(args.input, nodetype=int, create_using=nx.DiGraph())
 		for edge in G.edges():
@@ -109,7 +96,6 @@ def cluster_embedding_result(model):
 	print(y_pred)
 
 
-
 def main(args):
 	'''
 	Pipeline for representational learning for all nodes in a graph.
@@ -145,8 +131,6 @@ def main(args):
 	print(cos_similarity(model["17"], model["25"]))
 
 	# 对embedding之后的结果进行kmeans cluster
-
-
 
 
 if __name__ == "__main__":
