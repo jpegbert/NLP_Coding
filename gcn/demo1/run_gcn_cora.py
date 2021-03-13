b70ff0a1e15b73016678ece08a7cc8304ac04183
jiangpeng.jiang <jiangpeng.jiang@zhaopin.com.cn>
2021-03-13 18:59:35 +0800

    add gcn

---
+++ gcn/demo1/run_gcn_cora.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+from tensorflow.python.keras.callbacks import ModelCheckpoint
+from tensorflow.python.keras.optimizers import Adam
+from tensorflow.python.keras.layers import Lambda
+from tensorflow.python.keras.models import  Model
+from gcn.demo1.gcn1 import GCN
+from gcn.demo1.utils import preprocess_adj, plot_embeddings, load_data_v1
+
+if __name__ == "__main__":
+    FEATURE_LESS = False
+    A, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data_v1('cora')
+    A = preprocess_adj(A)
+    features /= features.sum(axis=1, ).reshape(-1, 1)
+    if FEATURE_LESS:
+        X = np.arange(A.shape[-1])
+        feature_dim = A.shape[-1]
+    else:
+        X = features
+        feature_dim = X.shape[-1]
+    model_input = [X, A]
+
+    # Compile model
+    model = GCN(A.shape[-1], feature_dim, 16, y_train.shape[1],  dropout_rate=0.5, l2_reg=2.5e-4,
+                feature_less=FEATURE_LESS, )
+    model.compile(optimizer=Adam(0.01), loss='categorical_crossentropy',
+                  weighted_metrics=['categorical_crossentropy', 'acc'])
+
+    NB_EPOCH = 200
+    PATIENCE = 200  # early stopping patience
+
+    val_data = (model_input, y_val, val_mask)
+    mc_callback = ModelCheckpoint('./best_model.h5',
+                                  monitor='val_weighted_categorical_crossentropy',
+                                  save_best_only=True,
+                                  save_weights_only=True)
+
+    # train
+    print("start training")
+    model.fit(model_input, y_train, sample_weight=train_mask, validation_data=val_data,
+              batch_size=A.shape[0], epochs=NB_EPOCH, shuffle=False, verbose=2, callbacks=[mc_callback])
+    # test
+    model.load_weights('./best_model.h5')
+    eval_results = model.evaluate(
+        model_input, y_test, sample_weight=test_mask, batch_size=A.shape[0])
+    print('Done.\n'
+          'Test loss: {}\n'
+          'Test weighted_loss: {}\n'
+          'Test accuracy: {}'.format(*eval_results))
+
+    embedding_model = Model(model.input, outputs=Lambda(lambda x: model.layers[-1].output)(model.input))
+    embedding_weights = embedding_model.predict(model_input, batch_size=A.shape[0])
+    y = np.genfromtxt("{}{}.content".format('./data/cora/', 'cora'), dtype=np.dtype(str))[:, -1]
+    plot_embeddings(embedding_weights, np.arange(A.shape[0]), y)
---
+++ gcn/demo1/utils.py
@@ -0,0 +1,198 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import networkx as nx
+import scipy.sparse as sp
+from sklearn.manifold import TSNE
+
+import os
+import pickle as pkl
+import sys
+
+
+def get_splits(y,):
+    idx_list = np.arange(len(y))
+    # train_val, idx_test = train_test_split(idx_list, test_size=0.2, random_state=1024)  # 1000
+    # idx_train, idx_val = train_test_split(train_val, test_size=0.2, random_state=1024)  # 500
+
+    idx_train = []
+    label_count = {}
+    for i, label in enumerate(y):
+        label = np.argmax(label)
+        if label_count.get(label, 0) < 20:
+            idx_train.append(i)
+            label_count[label] = label_count.get(label, 0) + 1
+
+    idx_val_test = list(set(idx_list) - set(idx_train))
+    idx_val = idx_val_test[0:500]
+    idx_test = idx_val_test[500:1500]
+
+    y_train = np.zeros(y.shape, dtype=np.int32)
+    y_val = np.zeros(y.shape, dtype=np.int32)
+    y_test = np.zeros(y.shape, dtype=np.int32)
+    y_train[idx_train] = y[idx_train]
+    y_val[idx_val] = y[idx_val]
+    y_test[idx_test] = y[idx_test]
+    train_mask = sample_mask(idx_train, y.shape[0])
+    val_mask = sample_mask(idx_val, y.shape[0])
+    test_mask = sample_mask(idx_test, y.shape[0])
+
+    return y_train, y_val, y_test, train_mask, val_mask, test_mask
+
+
+def load_data_v1(dataset="cora", path="./data/cora/",):
+
+    idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset), dtype=np.dtype(str))
+    features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+    onehot_labels = encode_onehot(idx_features_labels[:, -1])
+
+    # build graph
+    idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
+    idx_map = {j: i for i, j in enumerate(idx)}
+    edges_unordered = np.genfromtxt("{}{}.cites".format(path, dataset), dtype=np.int32)
+    edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
+                     dtype=np.int32).reshape(edges_unordered.shape)
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                        shape=(onehot_labels.shape[0], onehot_labels.shape[0]), dtype=np.float32)
+
+    # build symmetric adjacency matrix
+    # adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+    adj = convert_symmetric(adj, )
+
+    print('Dataset has {} nodes, {} edges, {} features.'.format(adj.shape[0], edges.shape[0], features.shape[1]))
+
+    y_train, y_val, y_test, train_mask, val_mask, test_mask = get_splits(onehot_labels)
+
+    return adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask
+
+
+def parse_index_file(filename):
+    """Parse index file."""
+    index = []
+    for line in open(filename):
+        index.append(int(line.strip()))
+    return index
+
+
+def load_data(dataset_str):
+    """Load data."""
+    FILE_PATH = os.path.abspath(__file__)
+    DIR_PATH = os.path.dirname(FILE_PATH)
+    DATA_PATH = os.path.join(DIR_PATH, 'data/')
+    DATA_PATH = "../data/cora/"
+
+    names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
+    objects = []
+    for i in range(len(names)):
+        with open("{}ind.{}.{}".format(DATA_PATH, dataset_str, names[i]), 'rb') as f:
+            if sys.version_info > (3, 0):
+                objects.append(pkl.load(f, encoding='latin1'))
+            else:
+                objects.append(pkl.load(f))
+
+    x, y, tx, ty, allx, ally, graph = tuple(objects)
+    test_idx_reorder = parse_index_file("{}ind.{}.test.index".format(DATA_PATH, dataset_str))
+    test_idx_range = np.sort(test_idx_reorder)
+
+    if dataset_str == 'citeseer':
+        # Fix citeseer dataset (there are some isolated nodes in the graph)
+        # Find isolated nodes, add them as zero-vecs into the right position
+        test_idx_range_full = range(min(test_idx_reorder),
+                                    max(test_idx_reorder) + 1)
+        tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
+        tx_extended[test_idx_range - min(test_idx_range), :] = tx
+        tx = tx_extended
+        ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
+        ty_extended[test_idx_range - min(test_idx_range), :] = ty
+        ty = ty_extended
+
+    features = sp.vstack((allx, tx)).tolil()
+    features[test_idx_reorder, :] = features[test_idx_range, :]
+    adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
+
+    labels = np.vstack((ally, ty))
+    labels[test_idx_reorder, :] = labels[test_idx_range, :]
+
+    idx_test = test_idx_range.tolist()
+    idx_train = range(len(y))
+    idx_val = range(len(y), len(y) + 500)
+
+    train_mask = sample_mask(idx_train, labels.shape[0])
+    val_mask = sample_mask(idx_val, labels.shape[0])
+    test_mask = sample_mask(idx_test, labels.shape[0])
+
+    y_train = np.zeros(labels.shape)
+    y_val = np.zeros(labels.shape)
+    y_test = np.zeros(labels.shape)
+    y_train[train_mask, :] = labels[train_mask, :]
+    y_val[val_mask, :] = labels[val_mask, :]
+    y_test[test_mask, :] = labels[test_mask, :]
+
+    return sp.csr_matrix(adj), features, y_train, y_val, y_test, train_mask, val_mask, test_mask
+
+
+def sample_mask(idx, l):
+    mask = np.zeros(l)
+    mask[idx] = 1
+    return np.array(mask, dtype=np.bool)
+
+
+def convert_symmetric(X, sparse=True):
+    if sparse:
+        X += X.T - sp.diags(X.diagonal())
+    else:
+        X += X.T - np.diag(X.diagonal())
+    return X
+
+
+def encode_onehot(labels):
+    classes = set(labels)
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
+    labels_onehot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
+    return labels_onehot
+
+
+def normalize_adj(adj, symmetric=True):
+    if symmetric:
+        d = sp.diags(np.power(np.array(adj.sum(1)), -0.5).flatten(), 0)
+        a_norm = adj.dot(d).transpose().dot(d).tocsr()
+    else:
+        d = sp.diags(np.power(np.array(adj.sum(1)), -1).flatten(), 0)
+        a_norm = d.dot(adj).tocsr()
+    return a_norm
+
+
+def preprocess_adj(adj, symmetric=True):
+    adj = adj + sp.eye(adj.shape[0])
+    adj = normalize_adj(adj, symmetric)
+    return adj
+
+
+def plot_embeddings(embeddings, X, Y):
+
+    emb_list = []
+    for k in X:
+        emb_list.append(embeddings[k])
+    emb_list = np.array(emb_list)
+
+    model = TSNE(n_components=2)
+    node_pos = model.fit_transform(emb_list)
+
+    color_idx = {}
+    for i in range(len(X)):
+        color_idx.setdefault(Y[i][:], [])
+        color_idx[Y[i][:]].append(i)
+
+    for c, idx in color_idx.items():
+        plt.scatter(node_pos[idx, 0], node_pos[idx, 1], label=c)
+    plt.legend()
+    plt.show()
+
+
+def preprocess_features(features):
+    """Row-normalize feature matrix and convert to tuple representation"""
+    rowsum = np.array(features.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    features = r_mat_inv.dot(features)
+    return features.todense()
